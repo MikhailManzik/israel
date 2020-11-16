9e66310cc66ea824e16878c62246a742a6d2ef54
MikhailManzik <mikhailmanzik@yandex.ru>
2020-11-16 17:31:11 +0200

    Изменяет название классов header.

--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,13 @@
 'use strict';
 
+var buttonCallback = document.querySelector('.header__callback');
+var popupCallback = document.querySelector('.popup-callback');
+
+var showPopup = function () {
+  popupCallback.classList.remove('popup--hidden');
+};
+
+buttonCallback.addEventListener('click', function (e) {
+  e.preventDefault();
+  showPopup();
+});
