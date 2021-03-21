0323bd9e6275f3695116394765b8b04be26c063f
Mikhail Manzik <mikhailmanzik@yandex.ru>
2021-03-22 01:12:28 +0200

    Добавялет валидацию для форм.

--- source/js/main.js
+++ source/js/main.js
@@ -13,16 +13,20 @@
   var allButtonClosePopups = document.querySelectorAll('.popup__close');
   var allOverlayPopups = document.querySelectorAll('.popup__overlay');
   var popup = document.querySelector('.popup');
-  var allInputsPopup = popup.querySelectorAll('input');
-  var inputName = popup.querySelector('#name');
-  var inputPhone = popup.querySelector('#phone');
+  var inputsName = document.querySelectorAll('.form__input-name');
   var checkboxBlock = popup.querySelector('.form__checkbox');
   var checkbox = document.querySelector('#checkbox');
   var tabsProgramsItems = document.querySelectorAll('.tabs__button');
   var tabsFaqItems = document.querySelectorAll('.faq__item');
+  var allForm = document.querySelectorAll('.form');
+  var inputsPhone = document.querySelectorAll('.form__input-phone');
+  var forEach = Array.prototype.forEach;
+  var advantagesSlider = document.querySelector('.advantages__cards');
+  var breakpoint = window.matchMedia('(min-width:767px)');
+  var swiper;
 
   var switchTabs = function (tabs) {
-    tabs.forEach(function (tab) {
+    forEach.call(tabs, function (tab) {
       tab.addEventListener('click', function () {
         var currentTab = document.querySelector('.tabs__button--active');
         var currentPane = document.querySelector('.desc__pane--active');
@@ -37,7 +41,7 @@
   };
 
   var switchTabsAccordion = function (tabs) {
-    tabs.forEach(function (tab) {
+    forEach.call(tabs, function (tab) {
       tab.addEventListener('click', function () {
         var currentTab = document.querySelector('.faq__item--active');
         currentTab.classList.remove('faq__item--active');
@@ -47,20 +51,9 @@
   };
 
   var addHandler = function (arrayElements) {
-    arrayElements.forEach(function (element) {
+    forEach.call(arrayElements, function (element) {
       element.addEventListener('click', closePopup);
     });
-  };
-
-  var checkValidityInputs = function (arrayElements) {
-    var counterValidInputs = 0;
-    arrayElements.forEach(function (element) {
-      if (element.checkValidity()) {
-        counterValidInputs += 1;
-      }
-      counterValidInputs -= 1;
-    });
-    return counterValidInputs >= 0 ? true : false;
   };
 
   var showPopupCallback = function () {
@@ -72,7 +65,6 @@
     var popupOpend = document.querySelector('.popup:not(.popup--hidden)');
     popupOpend.classList.add('popup--hidden');
     body.classList.remove('no-scroll');
-
   };
 
   var showPopupSuccess = function () {
@@ -108,107 +100,127 @@
     }
   };
 
-  var onOpenPopupSuccessClick = function (evt) {
-    evt.preventDefault();
-    if (checkValidityInputs(allInputsPopup) === true) {
-      showPopupSuccess();
+  var addPhoneMask = function () {
+    var maskOptions = {
+      mask: '+{7} (000) 000 00 00',
+    };
+
+    forEach.call(inputsPhone, function (input) {
+      var mask = new window.IMask(input, maskOptions);
+      return mask;
+    });
+  };
+
+  var validateNames = function () {
+    forEach.call(inputsName, function (name) {
+      name.addEventListener('invalid', function () {
+        if (name.validity.valid) {
+          name.classList.remove('error');
+        } else if (!name.validity.valid) {
+          name.classList.add('error');
+        } else if (name.validity.valueMissing) {
+          name.setCustomValidity('Обязательное поле');
+        } else {
+          name.setCustomValidity('');
+        }
+      });
+    });
+  };
+
+  var validatePhones = function () {
+    forEach.call(inputsPhone, function (phone) {
+      phone.addEventListener('invalid', function () {
+        if (!phone.validity.valid) {
+          phone.classList.add('error');
+        } else if (phone.value.tooShort) {
+          phone.setCustomValidity('Слишком короткий телефон');
+        } else if (phone.validity.valid) {
+          phone.classList.remove('error');
+        } else if (phone.validity.valueMissing) {
+          phone.setCustomValidity('Обязательное поле');
+        } else if (phone.validity.patternMismatch) {
+          phone.setCustomValidity('Ведите только цифры');
+        } else {
+          phone.setCustomValidity('');
+        }
+      });
+    });
+  };
+
+  var activateSlider = function () {
+    if (breakpoint.matches) {
+      if (swiper) {
+        swiper.destroy(true, true);
+      }
+      return;
+    } else if (!breakpoint.matches) {
+      showSliderAdvantagesSection();
     }
   };
 
-  inputName.addEventListener('input', function () {
-    if (inputName.validity.valid) {
-      inputName.classList.remove('error');
-    } else if (inputName.validity.valueMissing) {
-      inputName.setCustomValidity('Обязательное поле');
-    } else {
-      inputName.setCustomValidity('');
-    }
-  });
+  var showSliderAdvantagesSection = function () {
+    if (advantagesSlider) {
+      swiper = new window.Swiper(advantagesSlider, {
 
-  inputPhone.addEventListener('input', function () {
-    if (inputPhone.validity.valid) {
-      inputPhone.classList.remove('error');
-    } else if (inputPhone.validity.valueMissing) {
-      inputPhone.setCustomValidity('Обязательное поле');
-    } else {
-      inputPhone.setCustomValidity('');
-    }
-  });
+        loop: true,
 
+        pagination: {
+          el: '.buttons-slider',
+          clickable: true,
+        },
+
+        breakpoints: {
+          767: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          }
+        }
+      });
+    }
+  };
+
+  breakpoint.addListener(activateSlider);
+  activateSlider();
+
+  var feedbacksSlider = document.querySelector('.feedbacks__wrapper');
+
+  if (feedbacksSlider) {
+    feedbacksSlider = new window.Swiper(feedbacksSlider, {
+      loop: true,
+
+      pagination: {
+        el: '.page-counter',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
+  validateNames();
+  validatePhones();
+  addPhoneMask();
   buttonOpenPopup.addEventListener('click', onOpenPopupClick);
   document.addEventListener('keydown', onClosePopupPress);
-  buttonSubmit.addEventListener('click', onOpenPopupSuccessClick);
   buttonSuccess.addEventListener('click', onClosePopupClick);
   addHandler(allButtonClosePopups);
   addHandler(allOverlayPopups);
   buttonSubmit.addEventListener('click', addColorErrorChceckbox);
   checkboxBlock.addEventListener('input', removeColorErrorChceckbox);
 
-  buttonSubmit.addEventListener('click', function () {
-    if (!inputName.validity.valid) {
-      inputName.classList.add('error');
-    } if (!inputPhone.validity.valid) {
-      inputPhone.classList.add('error');
-    }
+  forEach.call(allForm, function (form) {
+    form.addEventListener('submit', function (evt) {
+      validateNames();
+      validatePhones();
+      evt.preventDefault();
+      showPopupSuccess();
+      form.reset();
+    });
   });
 
   switchTabs(tabsProgramsItems);
   switchTabsAccordion(tabsFaqItems);
+
 })();
-
-
-var advantagesSlider = document.querySelector('.advantages__cards');
-var breakpoint = window.matchMedia('(min-width:767px)');
-var swiper;
-
-var activateSlider = function () {
-  if (breakpoint.matches) {
-    if (swiper) {
-      swiper.destroy(true, true);
-    }
-    return;
-  } else if (!breakpoint.matches) {
-    showSliderAdvantagesSection();
-  }
-};
-
-var showSliderAdvantagesSection = function () {
-  if (advantagesSlider) {
-    swiper = new window.Swiper(advantagesSlider, {
-
-      loop: true,
-
-      pagination: {
-        el: '.buttons-slider',
-        clickable: true,
-      },
-
-      breakpoints: {
-        767: {
-          slidesPerView: 1,
-          slidesPerGroup: 1,
-        }
-      }
-    });
-  }
-};
-
-breakpoint.addListener(activateSlider);
-activateSlider();
-
-var feedbacksSlider = document.querySelector('.feedbacks__wrapper');
-
-if (feedbacksSlider) {
-  feedbacksSlider = new window.Swiper(feedbacksSlider, {
-    loop: true,
-
-    pagination: {
-      el: '.page-counter',
-      type: 'fraction',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-}
